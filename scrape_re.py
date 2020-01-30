61eb2f0eab6eb7acfbc039476471c14510385622
Masakazu Takahashi <masakaz@gmail.com>
2020-01-30 09:17:05 +0900

    First commit

---
+++ scrape_re.py
@@ -0,0 +1,22 @@
+import re
+from html import unescape
+from urllib.parse import urljoin
+
+with open('dp.html',encoding='utf-8_sig') as f:
+    html = f.read()
+
+#print(html)
+
+
+for partial_html in re.findall(r'<a itemprop="url".*?</ul>\s*</a></li>',html,re.DOTALL):
+    print(partial_html)
+    print('------')
+    url = re.search(r'<a itemprop="url" href="(.*?)">',partial_html).group(1)
+    url = urljoin('http://gijyo.jp/',url)
+    title = re.search(r'<p itemprop="name".*?</p>',partial_html).group(0)
+    title = title.replace('<br/>',' ')
+    title = re.sub(r'<.*?>','',title)
+    title = unescape(title)
+
+    print(url,title)
+
