4496f47ec1599e7b93398365a01423ddc12c2168
Masakazu Takahashi <masakaz@gmail.com>
2020-01-30 13:38:54 +0900

    文字コード変換対応

--- requests_header_encoding.py
+++ requests_header_encoding.py
@@ -3,6 +3,8 @@ import requests
 
 url = sys.argv[1]
 r = requests.get(url)
+r_temp = r.text
+r2 = r_temp.replace('\xa9','') #変換できない文字を置換
 print(f'encoding: {r.encoding}',file=sys.stderr)
-print(r.text)
+print(r2)
 
